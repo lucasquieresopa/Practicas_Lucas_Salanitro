9aa66280472c83089757a157df337ed5f09a996a
Lucas Salanitro <lucassalanitro32@gmail.com>
2022-04-04 10:19:15 -0300

    Agrego practica 2 ayed

--- Practica2/src/Act2/Ej2_1.java
+++
@@ -1,9 +0,0 @@
-package Act2;
-import tp02.ejercicio1.*;
-public class Ej2_1 {
-
-	public static void main(String[]args) {
-		
-		ListaDeEnteros<ListaDeEnterosEnlazada> lista1 = new ListaDeEnteros<ListaDeEnterosEnlazada>();
-	}
-}
